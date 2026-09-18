209876b02fd9bb5004376556efd7bba21ed665fa
hergo-1 <hergo-1@users.noreply.github.com>
2026-09-18 11:05:10 +0000

    Fix build: add room-paging artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,6 +112,7 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-paging:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
 
     implementation("androidx.paging:paging-runtime:3.2.1")
